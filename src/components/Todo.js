192dea5ef9cc315b920a66fd47960dd5ce5cad6d
D3stros <patrick.fallmann@hotmail.com>
2022-08-18 20:20:13 +0200

    Adding open todos and delete functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,30 +1,31 @@
 import React from 'react';
-import { useState } from 'react';
 
-const Todo = ({ description, done }) => {
-	const [isdone, setDone] = useState(done);
-
-	const changeTodo = () => {
-		if (isdone) {
-			setDone(false);
-		} else {
-			setDone(true);
-		}
-	};
-
+const Todo = ({ description, done, onChangeTodo, onDeleteTodo, index }) => {
 	return (
 		<div>
 			<div
 				className={
-					isdone
+					done
 						? 'flex justify-between items-center p-2 bg-green-600 text-white'
 						: 'flex justify-between items-center p-2 bg-red-500 text-white'
 				}
 			>
-				<h1 className='text-lg cursor-pointer' onClick={changeTodo}>
+				<h1
+					className='text-lg cursor-pointer'
+					onClick={() => {
+						onChangeTodo(index);
+					}}
+				>
 					{description}
 				</h1>
-				<button className='text-lg bg-gray-400 p-2 text-white'>Delete</button>
+				<button
+					className='text-lg bg-gray-400 p-2 text-white'
+					onClick={() => {
+						onDeleteTodo(index);
+					}}
+				>
+					Delete
+				</button>
 			</div>
 		</div>
 	);
