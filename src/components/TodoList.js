c7a5b7e7090a54748865693956125e931764d991
D3stros <patrick.fallmann@hotmail.com>
2022-08-18 20:39:07 +0200

    Adding create functionality

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -11,6 +11,17 @@ const alltodos = [
 const TodoList = () => {
 	const [opencount, countOpenTodos] = useState(0);
 	const [todos, setTodos] = useState(alltodos);
+	const [textinput, settextinput] = useState('');
+
+	const changeText = (e) => {
+		settextinput(e.target.value);
+	};
+
+	const submit = (e) => {
+		e.preventDefault();
+		const newTodos = [...todos, { description: textinput, done: false }];
+		setTodos(newTodos);
+	};
 
 	const countOpen = () => {
 		const donetodos = todos.filter((item) => {
@@ -41,9 +52,23 @@ const TodoList = () => {
 
 	return (
 		<div className='shadow-sm hover:shadow-lg'>
-			<div className='text-center bg-gray-900 text-white text-3xl py-4 font-semibold'>
-				<h1>Our todos</h1>
+			<div className='text-center bg-gray-900 text-white py-4 font-semibold'>
+				<h1 className='text-3xl'>Our todos</h1>
 				<h2>Open todos: {opencount}</h2>
+				<form className='grid grid-cols-3 py-2'>
+					<input
+						onChange={changeText}
+						type='text'
+						placeholder='New Todo...'
+						className='col-span-2 py-2 text-gray-900'
+					></input>
+					<input
+						onClick={submit}
+						type='submit'
+						value='Add todo'
+						className='col-span-1 text-white-900 cursor-pointer'
+					></input>
+				</form>
 			</div>
 			{todos.map((item, index) => {
 				return (
