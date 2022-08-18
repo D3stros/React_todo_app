4436574eddf84ec9064563b08febc8781ced8a2f
D3stros <patrick.fallmann@hotmail.com>
2022-08-18 16:57:37 +0200

    Using useState for style by state for todo elements

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Todo = ({ description, done }) => {
+	const [isdone, setDone] = useState(done);
+
 	const changeTodo = () => {
-		done = !done;
+		if (isdone) {
+			setDone(false);
+		} else {
+			setDone(true);
+		}
 	};
 
 	return (
 		<div>
 			<div
 				className={
-					done
+					isdone
 						? 'flex justify-between items-center p-2 bg-green-600 text-white'
 						: 'flex justify-between items-center p-2 bg-red-500 text-white'
 				}
 			>
-				<h1 className='text-lg cursor-pointer'>{description}</h1>
+				<h1 className='text-lg cursor-pointer' onClick={changeTodo}>
+					{description}
+				</h1>
 				<button className='text-lg bg-gray-400 p-2 text-white'>Delete</button>
 			</div>
 		</div>
