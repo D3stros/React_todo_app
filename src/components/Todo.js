73380eab061f5070b7de0dfb02372f38c588d585
D3stros <patrick.fallmann@hotmail.com>
2022-08-18 16:48:17 +0200

    Adding tailwind css style

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const Todo = ({ description, done }) => {
+	const changeTodo = () => {
+		done = !done;
+	};
+
 	return (
 		<div>
-			<h1>{description}</h1>
+			<div
+				className={
+					done
+						? 'flex justify-between items-center p-2 bg-green-600 text-white'
+						: 'flex justify-between items-center p-2 bg-red-500 text-white'
+				}
+			>
+				<h1 className='text-lg cursor-pointer'>{description}</h1>
+				<button className='text-lg bg-gray-400 p-2 text-white'>Delete</button>
+			</div>
 		</div>
 	);
 };
