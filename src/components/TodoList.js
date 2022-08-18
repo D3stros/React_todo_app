6c722dee66e57f2ae86b7809d2153a5ec5b084b6
D3stros <patrick.fallmann@hotmail.com>
2022-08-18 17:42:05 +0200

    Adding useeffect hook

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Todo from './Todo';
+import { useState, useEffect } from 'react';
 
 const todos = [
 	{ description: 'Shopping', done: true },
@@ -8,10 +9,24 @@ const todos = [
 ];
 
 const TodoList = () => {
+	const [opencount, countOpenTodos] = useState(0);
+
+	const countOpen = () => {
+		const donetodos = todos.filter((item) => {
+			return !item.done;
+		});
+		countOpenTodos(donetodos.length);
+	};
+
+	useEffect(() => {
+		countOpen();
+	}, [opencount]);
+
 	return (
 		<div className='shadow-sm hover:shadow-lg'>
 			<div className='text-center bg-gray-900 text-white text-3xl py-4 font-semibold'>
 				<h1>Our todos</h1>
+				<h2>Open todos: {opencount}</h2>
 			</div>
 			{todos.map((item, index) => {
 				return (
